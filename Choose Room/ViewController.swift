7a33e942cae39ffdceb77ca2b5e2b01c8064f5f1
Adolfho Athyla <adolfo.athyla@gmail.com>
2016-12-02 12:08:24 -0300

    Initial Commit

---
+++ Choose Room/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Choose Room
+//
+//  Created by Adolfho Athyla on 02/12/16.
+//  Copyright © 2016 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
